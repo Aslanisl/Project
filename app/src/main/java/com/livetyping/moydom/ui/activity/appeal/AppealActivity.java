9467ee1077a746a64d60ef4f54bb0614ae67ce1a
Ivan <aslanisl@mail.ru>
2017-12-12 17:46:39 +0300

    add url and call to appeal. added screen for appeal

---
+++ app/src/main/java/com/livetyping/moydom/ui/activity/appeal/AppealActivity.java
@@ -0,0 +1,92 @@
+package com.livetyping.moydom.ui.activity.appeal;
+
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.livetyping.moydom.R;
+import com.livetyping.moydom.api.Api;
+import com.livetyping.moydom.api.ApiUrlService;
+import com.livetyping.moydom.api.CallbackWrapper;
+import com.livetyping.moydom.api.ServerCallback;
+import com.livetyping.moydom.apiModel.BaseModel;
+import com.livetyping.moydom.ui.activity.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class AppealActivity extends BaseActivity implements ServerCallback{
+
+    @BindView(R.id.toolbar) Toolbar mToolbar;
+    private CompositeDisposable mCompositeDisposable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appeal);
+        ButterKnife.bind(this);
+        initToolBar();
+        initCategories();
+    }
+
+    private void initToolBar(){
+        mToolbar.setNavigationIcon(R.drawable.close);
+        mToolbar.setTitle(R.string.appeal);
+        setSupportActionBar(mToolbar);
+        mToolbar.setNavigationOnClickListener(view -> onBackPressed());
+    }
+
+    private void initCategories(){
+        mCompositeDisposable = new CompositeDisposable();
+        mCompositeDisposable.add(Api.getApiService().getAddresses(ApiUrlService.getAddressesUrl())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new CallbackWrapper<BaseModel>(this){
+                    @Override
+                    protected void onSuccess(BaseModel baseModel) {
+
+                    }
+                }));
+    }
+
+    @Override
+    public void onUnknownError(String error) {
+
+    }
+
+    @Override
+    public void onTimeout() {
+
+    }
+
+    @Override
+    public void onNetworkError() {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.send_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_send:
+                //TODO send appeal
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mCompositeDisposable != null && !mCompositeDisposable.isDisposed()) mCompositeDisposable.dispose();
+    }
+}
