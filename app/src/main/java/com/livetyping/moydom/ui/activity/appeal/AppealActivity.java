0f88936026ac73ba4b79499eb40cb27b01871ca4
Иван Катаев [Freelancer] <aslanisl@mail.ru>
2017-12-13 22:51:45 +0300

    work with add photo to appeal

--- app/src/main/java/com/livetyping/moydom/ui/activity/appeal/AppealActivity.java
+++ app/src/main/java/com/livetyping/moydom/ui/activity/appeal/AppealActivity.java
@@ -1,12 +1,21 @@
 package com.livetyping.moydom.ui.activity.appeal;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.livetyping.moydom.BuildConfig;
 import com.livetyping.moydom.R;
 import com.livetyping.moydom.api.Api;
 import com.livetyping.moydom.api.ApiUrlService;
@@ -14,7 +23,9 @@ import com.livetyping.moydom.api.CallbackWrapper;
 import com.livetyping.moydom.apiModel.appeal.AppealModel;
 import com.livetyping.moydom.apiModel.appeal.AppealResponse;
 import com.livetyping.moydom.ui.activity.BaseActivity;
+import com.livetyping.moydom.utils.HelpUtils;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,15 +36,18 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
 
-public class AppealActivity extends BaseActivity {
+public class AppealActivity extends BaseActivity implements AppealPhotoSelectorFragment.AppealPhotoSelectorListener{
 
     private static final int REQUEST_CODE_SELECT_CATEGORY = 1;
+    private static final int REQUEST_CODE_AVATAR_FROM_CAMERA = 2;
 
     @BindView(R.id.toolbar) Toolbar mToolbar;
     @BindView(R.id.activity_appeal_categories) TextView mCategoryName;
     private CompositeDisposable mCompositeDisposable;
     private ArrayList<AppealModel> mCategories = new ArrayList<>();
     private AppealModel mSelectedModel;
+
+    private List<File> mPhotoFiles = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,6 +111,41 @@ public class AppealActivity extends BaseActivity {
         }
     }
 
+    @OnClick(R.id.activity_appeal_add_photo)
+    void addPhoto(){
+        AppealPhotoSelectorFragment fragment = new AppealPhotoSelectorFragment();
+        fragment.show(getSupportFragmentManager(), AppealPhotoSelectorFragment.TAG);
+    }
+
+    @Override
+    public void fromFile() {
+
+    }
+
+    @Override
+    public void fromCamera() {
+        if (checkCameraHardware()) {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                File file = new File(
+                        getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+                        "photo_" + mPhotoFiles.size() + ".png"
+                );
+                String providerAuthority = BuildConfig.APPLICATION_ID + ".fileprovider";
+                Uri photoURI = FileProvider.getUriForFile(this, providerAuthority, file);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_CODE_AVATAR_FROM_CAMERA);
+            }
+        } else {
+            showToast(R.string.no_camera);
+        }
+    }
+
+    private boolean checkCameraHardware() {
+        return (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) ||
+                (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH));
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -104,6 +153,11 @@ public class AppealActivity extends BaseActivity {
             mSelectedModel = data.getParcelableExtra("category");
             if (mSelectedModel != null){
                 mCategoryName.setText(mSelectedModel.getName());
+            }
+        } else if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_AVATAR_FROM_CAMERA){
+            Uri imageUri = data.getData();
+            if (imageUri != null) {
+                mPhotoFiles.add(new File(imageUri.getPath()));
             }
         }
     }
