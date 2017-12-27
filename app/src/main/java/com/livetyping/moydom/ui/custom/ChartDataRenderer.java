3fa6a33deb3a6820cd055a26b79a132d08267c18
MrXlebNick <xlebnikne@list.ru>
2017-12-27 14:19:37 +0300

    ChartDataRenderer that was missing is now added

---
+++ app/src/main/java/com/livetyping/moydom/ui/custom/ChartDataRenderer.java
@@ -0,0 +1,106 @@
+package com.livetyping.moydom.ui.custom;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.os.Build;
+
+import com.github.mikephil.charting.animation.ChartAnimator;
+import com.github.mikephil.charting.buffer.BarBuffer;
+import com.github.mikephil.charting.interfaces.dataprovider.BarDataProvider;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.renderer.BarChartRenderer;
+import com.github.mikephil.charting.utils.Transformer;
+import com.github.mikephil.charting.utils.Utils;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+/**
+ * Created by XlebNick for MoyDom.
+ */
+
+public class ChartDataRenderer extends BarChartRenderer {
+    public ChartDataRenderer(BarDataProvider chart, ChartAnimator animator, ViewPortHandler viewPortHandler) {
+        super(chart, animator, viewPortHandler);
+    }
+    @Override
+    protected void drawDataSet(Canvas c, IBarDataSet dataSet, int index) {
+
+        Transformer trans = mChart.getTransformer(dataSet.getAxisDependency());
+
+        mBarBorderPaint.setColor(dataSet.getBarBorderColor());
+        mBarBorderPaint.setStrokeWidth(Utils.convertDpToPixel(dataSet.getBarBorderWidth()));
+
+        final boolean drawBorder = dataSet.getBarBorderWidth() > 0.f;
+
+        float phaseX = mAnimator.getPhaseX();
+        float phaseY = mAnimator.getPhaseY();
+
+        // initialize the buffer
+        BarBuffer buffer = mBarBuffers[index];
+        buffer.setPhases(phaseX, phaseY);
+        buffer.setDataSet(index);
+        buffer.setInverted(mChart.isInverted(dataSet.getAxisDependency()));
+        buffer.setBarWidth(mChart.getBarData().getBarWidth());
+
+        buffer.feed(dataSet);
+
+        trans.pointValuesToPixel(buffer.buffer);
+
+        final boolean isSingleColor = dataSet.getColors().size() == 1;
+
+        if (isSingleColor) {
+            mRenderPaint.setColor(dataSet.getColor());
+        }
+
+        LinearGradient gradient;
+        int color1 = 0, color2 = 0;
+        for (int j = 0; j < buffer.size(); j += 4) {
+
+            if (!mViewPortHandler.isInBoundsLeft(buffer.buffer[j + 2]))
+                continue;
+
+            if (!mViewPortHandler.isInBoundsRight(buffer.buffer[j]))
+                break;
+
+
+            if (!isSingleColor) {
+                // Set the color for the currently drawn value. If the index
+                // is out of bounds, reuse colors.
+                mRenderPaint.setColor(dataSet.getColor(j / 4));
+                if (dataSet.getColor(j / 4) == Color.parseColor("#ff5b91")){
+                    color1 = Color.parseColor("#d23285");
+                    color2 = Color.parseColor("#ff5b91");
+
+                } else  if (dataSet.getColor(j / 4) == Color.parseColor("#343d94")){
+                    color1 = Color.parseColor("#6d2dd3");
+                    color2 = Color.parseColor("#343d94");
+
+                } else  if (dataSet.getColor(j / 4) == Color.parseColor("#ffc13c")){
+                    color1 = Color.parseColor("#ff7e69");
+                    color2 = Color.parseColor("#ffc13c");
+
+                }
+
+            }
+            gradient = new LinearGradient(buffer.buffer[j], buffer.buffer[j + 1], buffer.buffer[j + 2],
+                    buffer.buffer[j + 3], color1, color2, Shader.TileMode.MIRROR);
+            mRenderPaint.setShader(gradient);
+
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                c.drawRoundRect(buffer.buffer[j], buffer.buffer[j + 1], buffer.buffer[j + 2],
+                        buffer.buffer[j + 3], 1, 1, mRenderPaint);
+            } else {
+
+                c.drawRect(buffer.buffer[j], buffer.buffer[j + 1], buffer.buffer[j + 2],
+                        buffer.buffer[j + 3], mRenderPaint);
+            }
+
+            if (drawBorder) {
+                c.drawRect(buffer.buffer[j], buffer.buffer[j + 1], buffer.buffer[j + 2],
+                        buffer.buffer[j + 3], mBarBorderPaint);
+            }
+        }
+    }
+}
